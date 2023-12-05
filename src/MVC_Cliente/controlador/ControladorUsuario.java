c008918168642e82fada689f6c7f87484ed50f7f
Saul-Eduardo <112462894+Saul-Eduardo@users.noreply.github.com>
2023-12-05 12:03:14 -0600

    Añadir clases en cada paquete Cliente

--- src/MVC_Cliente/controlador/ControladorUsuario.java
+++ src/MVC_Cliente/controlador/ControladorUsuario.java
@@ -1,39 +1,27 @@
 
 package MVC_Cliente.controlador;
 
+
 import java.util.ArrayList;
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
 import MVC_Cliente.modelo.Usuario;
+import MVC_Cliente.modelo.ArchivoUsuarios;
 
 public class ControladorUsuario {
     private ArrayList<Usuario> listaUsuarios;
     private ArrayList<String> usuariosConectados;
+    private ArchivoUsuarios archivoUsuarios;
 
     public ControladorUsuario(String rutaArchivo) {
         this.listaUsuarios = new ArrayList<>();
         this.usuariosConectados = new ArrayList<>();
-        cargarUsuariosDesdeArchivo(rutaArchivo);
+        this.archivoUsuarios = new ArchivoUsuarios(rutaArchivo);
+        cargarUsuariosDesdeArchivo();
     }
 
-    private void cargarUsuariosDesdeArchivo(String rutaArchivo) {
-        try (BufferedReader lector = new BufferedReader(new FileReader(rutaArchivo))) {
-            String linea;
-            while ((linea = lector.readLine()) != null) {
-                // Formato de Usuario "nombreUsuario,contraseña" 
-                String[] partes = linea.split(",");
-                if (partes.length == 2) {
-                    String nombreUsuario = partes[0];
-                    String contraseña = partes[1];
-                    listaUsuarios.add(new Usuario(nombreUsuario, contraseña));
-                }
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+    private void cargarUsuariosDesdeArchivo() {
+        listaUsuarios = archivoUsuarios.cargarUsuarios();
     }
-        
+
     public boolean registrarUsuario(String nombreUsuario, String contraseña) {
         // Verificar si el usuario ya existe
         for (Usuario usuario : listaUsuarios) {
@@ -45,6 +33,7 @@ public class ControladorUsuario {
         // Crear un nuevo usuario y agregarlo a la lista
         Usuario nuevoUsuario = new Usuario(nombreUsuario, contraseña);
         listaUsuarios.add(nuevoUsuario);
+        // Puedes agregar código para guardar la lista actualizada en el archivo aquí
         return true;
     }
 
@@ -56,7 +45,6 @@ public class ControladorUsuario {
                 return true;  // Autenticación exitosa
             }
         }
-
         return false;  // Autenticación fallida
     }
 
@@ -70,3 +58,4 @@ public class ControladorUsuario {
         return usuariosConectados;
     }
 }
+
