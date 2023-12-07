4b2dd89e9f5838d64f6f74ad1658ef8e381d0b3d
Saul-Eduardo <112462894+Saul-Eduardo@users.noreply.github.com>
2023-12-07 12:15:43 -0600

    Solucion Error Hidratación

--- src/Cliente/controlador/ControladorUsuario.java
+++ src/Cliente/controlador/ControladorUsuario.java
@@ -36,6 +36,19 @@ public class ControladorUsuario {
             return usuariosH;
         }catch(FileNotFoundException sad){
            System.out.println("archivo no encontrado");
+            // Generar un archivo serializado con un usuario predeterminado
+            Usuario usuarioPredeterminado = new Usuario("hola", "hola");
+            ArrayList<Usuario> usuariosNuevos = new ArrayList<>();
+            usuariosNuevos.add(usuarioPredeterminado);
+
+            try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("databaseusers.ser"))) {
+                oos.writeObject(usuariosNuevos);
+                System.out.println("Nuevo archivo serializado.");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            // Volver a intentar hidratar
+            return hidratar();
         }catch(IOException ert){
             System.out.println("entrada o salida error");
         }catch(ClassNotFoundException dor){
